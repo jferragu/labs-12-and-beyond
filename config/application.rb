8b8666449ffde7196f1ba2164ddee8bbe445b5b1
Jacob Ferragut <jferragu@uncc.edu>
2020-03-28 02:37:16 +0000

    Set up add_entryfeature scenario 1

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Blog
     config.web_console.whitelisted_ips = '198.86.76.7'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.web_console.development_only = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
